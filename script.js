8ae52c91c04dfb781047eb57a47fa92c3342b0a3
SaschaRapp <50624095+SaschaRapp@users.noreply.github.com>
2025-08-05 16:19:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@
   window.setLanguage = setLang; // Make it globally accessible
 
   const translations = {
+    
+    //Home-Page Navigation
     en: {
       work: "Work",
       video: "Video",
@@ -30,8 +32,25 @@
       contact: "Kontakt",
       view_all: "alle anzeigen"
     }
+
+    // Levanto Project
+    en: {
+    // ...
+    levanto_title: "LEVANTO",
+    levanto_subtitle: "A Weekend Surf Trip To Italy",
+    levanto_description: "I've had this footage sitting around because I could never quite figure out a story. This trip, however, was unforgettable. Lots of firsts this surf trip: first trip to Italy, first surf trip for a weekend in Europe, first surf with a broken hand. So I've decided to simply cut together some of my favourite moments that I managed to document. Hopefully encapsulating the absolute vibe the 5 of us experienced, on a spontaneous strike mission to Levanto for that killer storm swell."
+  },
+  de: {
+    // ...
+    levanto_title: "LEVANTO",
+    levanto_subtitle: "Ein Surfwochenende in Italien",
+    levanto_description: "Ich hatte das Filmmaterial schon eine ganze Weile rumliegen, weil ich nie so richtig wusste, wie ich daraus eine Geschichte machen soll. Aber dieser Trip war unvergesslich. Viele Premieren auf diesem Surftrip: erstes Mal in Italien, erstes Surfwochenende in Europa, erstes Mal Surfen mit gebrochener Hand. Deshalb habe ich einfach einige meiner Lieblingsmomente zusammengeschnitten, die ich festhalten konnte – in der Hoffnung, die Stimmung einzufangen, die wir fünf auf unserem spontanen Strike-Mission-Trip nach Levanto während dieses epischen Sturms erlebt haben."
+  }
+};
+    
   };
 
   setLang(storedLang);
 })();
+
 
